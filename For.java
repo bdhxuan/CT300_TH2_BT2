e6c3d807a3440e26c73b5a6631fcf5bf22d885df
Xuan Bui <xuanb1910180@student.ctu.edu.vn>
2023-02-22 15:44:58 +0700

    added For.java

---
+++ For.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class KiemTraSoNguyenTo
+{
+    public static void main (String[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        System.out.println(i);
+    }
+}
